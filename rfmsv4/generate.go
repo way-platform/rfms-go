54652082785443b45a77b955fb10bf0ca371d46b
Oscar Söderlund <odsod@users.noreply.github.com>
2025-04-28 15:09:53 +0200

    chore(rfmsv4): update file layout

--- rfmsv4/generate.go
+++ rfmsv4/generate.go
@@ -1,3 +1,4 @@
 package rfmsv4
 
-//go:generate go tool oapi-codegen -config cfg.yaml spec.yaml
+//go:generate cp rFMS_4_0.original.yaml api.yaml
+//go:generate go tool oapi-codegen -config cfg.yaml api.yaml
