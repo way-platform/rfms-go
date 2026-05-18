6a1d14ea49f81c6c7be287a9f8a8665e895f8baa
Oscar Söderlund <odsod@users.noreply.github.com>
2026-05-18 09:39:44 +0200

    refactor: remove Error struct, use plain connect errors
    - Remove exported Error type (breaking change for errors.As consumers)
    - Switch to simple vw-go pattern: connect.NewError with descriptive message
    - Consumers should use connect.CodeOf(err) instead of errors.As

--- error.go
+++ error.go
@@ -2,91 +2,37 @@ package rfms
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"net/http"
-	"strconv"
-	"time"
 
 	"connectrpc.com/connect"
 	"github.com/way-platform/rfms-go/internal/openapi/rfmsv4oapi"
 )
 
-// Error is an error returned by the rFMS API.
-type Error struct {
-	// Method is the HTTP method used to make the request.
-	Method string
-	// URL is the URL of the request.
-	URL string
-	// Status is the HTTP status.
-	Status string
-	// StatusCode is the HTTP status code.
-	StatusCode int
-	// RateLimitReset is the duration until the rate limit is reset.
-	RateLimitReset time.Duration
-	// Identifier of the rFMS error.
-	Identifier string
-	// Description of the rFMS error.
-	Description string
-	// ErrorURI provides more information about the error.
-	ErrorURI string
-}
-
 func newHTTPError(resp *http.Response) error {
-	result := &Error{
-		Method:     resp.Request.Method,
-		URL:        resp.Request.URL.String(),
-		Status:     resp.Status,
-		StatusCode: resp.StatusCode,
-	}
+	var msg string
 	if data, err := io.ReadAll(resp.Body); err == nil {
 		var body rfmsv4oapi.ErrorObject
 		if err := json.Unmarshal(data, &body); err == nil {
+			parts := fmt.Sprintf("http %d", resp.StatusCode)
 			if body.Error != nil {
-				result.Identifier = *body.Error
+				parts += ": " + *body.Error
 			}
 			if body.ErrorDescription != nil {
-				result.Description = *body.ErrorDescription
+				parts += " - " + *body.ErrorDescription
 			}
-			if body.ErrorURI != nil {
-				result.ErrorURI = *body.ErrorURI
-			}
+			msg = parts
+		} else if len(data) > 0 {
+			msg = fmt.Sprintf("http %d: %s", resp.StatusCode, data)
 		} else {
-			result.Description = string(data)
+			msg = fmt.Sprintf("http %d", resp.StatusCode)
 		}
+	} else {
+		msg = fmt.Sprintf("http %d", resp.StatusCode)
 	}
-	if retryAfterHeader := resp.Header.Get("Retry-After"); retryAfterHeader != "" {
-		if retryAfter, err := strconv.Atoi(retryAfterHeader); err == nil {
-			result.RateLimitReset = time.Duration(retryAfter) * time.Second
-		}
-	}
-	return result.connectError()
-}
-
-// Error implements the error interface.
-func (e *Error) Error() string {
-	if e.RateLimitReset > 0 {
-		return fmt.Sprintf(
-			"%s %s - %s: rate limit reset in %s",
-			e.Method,
-			e.URL,
-			e.Status,
-			e.RateLimitReset,
-		)
-	}
-	return fmt.Sprintf(
-		"%s %s: %s: %s - %s %s",
-		e.Method,
-		e.URL,
-		e.Status,
-		e.Identifier,
-		e.Description,
-		e.ErrorURI,
-	)
-}
-
-func (e *Error) connectError() error {
-	return connect.NewError(httpStatusToConnectCode(e.StatusCode), e)
+	return connect.NewError(httpStatusToConnectCode(resp.StatusCode), errors.New(msg))
 }
 
 func httpStatusToConnectCode(statusCode int) connect.Code {
